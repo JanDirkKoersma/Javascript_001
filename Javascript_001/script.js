8dee82a5d415b530646973c1f583e2177d4f1a35
JanDirkKoersma <75176681+JanDirkKoersma@users.noreply.github.com>
2021-03-05 09:22:38 +0100

    Add files via upload

---
+++ Javascript_001/script.js
@@ -0,0 +1,35 @@
+console.log("Hello Winc Academy");
+
+let name = "Jan Dirk";
+console.log(name);
+
+var number = 4;
+number = number + 4;
+console.log(number);
+
+var number = "4";
+number = number + "4";
+console.log(number);
+
+var number = 100;
+number = number + 100;
+console.log(number);
+
+var number = 200;
+number = number - 100;
+console.log(number);
+
+var number = 20;
+number = number * 20;
+console.log(number);
+
+var number = 1000;
+number = number / 100;
+console.log(number);
+
+var number = 5;
+number = number % 2;
+console.log(number);
+
+let age = 48;
+console.log(age);
